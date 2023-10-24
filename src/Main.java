1178105cd9cb1c4c226e07d7e34cff35147c7549
unknown <memahmud.mehedi99@gmail.com>
2023-10-25 04:40:22 +0600

    finished user and account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import Account.UserAccountManager;
+import Executions.Execution;
+import User.User;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Execution execution = new Execution();
+
+        UserAccountManager userAccountManager = new UserAccountManager("users.txt");
+
+        boolean session = true;
+        User loggedInUser = null;
+        Scanner scanner = new Scanner(System.in);
+
+        while(session){
+
+            execution.showMenu();
+            int input = scanner.nextInt();
+
+            if(input == 1){
+                loggedInUser = execution.executeLogin(scanner, userAccountManager);
+                execution.handleUserOperations(loggedInUser, scanner, userAccountManager);
+            } else if(input == 2){
+                loggedInUser = execution.executeRegister(scanner, userAccountManager);
+                execution.handleUserOperations(loggedInUser, scanner, userAccountManager);
+            } else if(input == 3){
+                System.out.println("Exiting...");
+                session = false;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
 
     }
 }
